cc56a07ce5468dcb887dd1696ff559b38ba43537
cargillb <56117629+cargillb@users.noreply.github.com>
2020-02-19 09:40:22 -0500

    setup changes

---
+++ projects/cargillb/Assignment4/test_classroom_manager.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+
+class TestStudent(TestCase):
+    def test_get_full_name(self):
+        self.fail()
+
+    def test_submit_assignment(self):
+        self.fail()
+
+    def test_get_assignments(self):
+        self.fail()
+
+    def test_get_assignment(self):
+        self.fail()
+
+    def test_get_average(self):
+        self.fail()
+
+    def test_remove_assignment(self):
+        self.fail()
+
+
+class TestAssignment(TestCase):
+    def test_assign_grade(self):
+        self.fail()
